638535c694223c90db44aa0872878453aade1ae2
Lucianeleite <65182332+Lucianeleite@users.noreply.github.com>
2024-10-08 21:46:32 -0300

    Add files via upload

---
+++ aula02_031024.py
@@ -0,0 +1,69 @@
+# print('Quero pipoca') #Quero comer pipoca.
+# print("testet teetehjsdjhsgf fgh sf")
+# '''
+# não mostar isso
+# '''
+# print(123)
+# print( 37438,1545)
+# print(mmjknnjnnn)
+# print(jhjhfjshfjsh)
+# print(true)
+# print(False)
+# Aula 04/10/24
+
+# idade=20
+# idade1=13
+# idade2=21
+# if idade1>=18:
+#     print("Você é maior de idadae")
+# else:
+#     print("Você é menor de idadae")
+
+# temperatura1=36
+# temperatura2=40
+# temperatura3=37
+# print(Temperatura1)
+
+
+# if Temperatura<37,5:
+#     print("Você não está com febre")
+# elif Temperatura==37,5:
+#     print("Você está febril")
+# else:
+#     print("Você está com febre")
+
+
+# idade=13
+
+# if idade<16:
+#     print("Menor de Idade")
+# elif idade>=16 or idade<=18:
+#     print("aguarde")
+# else: 
+#     print("Maior de idade")
+
+
+###COMPARATIVOS NO PRINT###
+
+fruta1='jambo'
+fruta2='maçã'
+fruta3='uva'
+fruta4='maçã'
+num1=67
+num2=12
+num3=12.0
+num4=67.1
+
+print(fruta1==fruta3)
+fruta1=fruta3
+print(fruta1)
+
+print(fruta1==fruta4)
+print(fruta2==fruta4)
+print(fruta3==fruta4)
+print(num1==num2)
+print(num1!=num2)
+print(num1>num2)
+print(num1<num2)
+print(num2==num3)
+print(num1!=num4)
